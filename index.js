dd39c61dd00481d1d36d02b1ec21a0138ade6ab5
leeevi <leventekecskemeti@gmail.com>
2023-08-24 20:13:02 +0200

    first

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const port = process.env.PORT || PORT;
 
+app.get("/client.zip", (req, res) => {
+  const filePath = path.join(__dirname, "public", "client.zip");
+  res.setHeader("Content-Type", "application/zip");
+  res.setHeader("Content-Disposition", "attachment; filename=client.zip");
+  res.setHeader("Content-Length", fs.statSync(filePath).size);
+  res.sendFile(filePath);
+});
+
 app.listen(port, () => {
   console.log("server running on port 3003");
 });
